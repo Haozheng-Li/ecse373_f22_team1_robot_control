dbd4d93bad8c23bc8ed6bfdc91d43da2eb00807c
Haozheng-Li <haozheng.l@outlook.com>
2022-10-10 23:35:21 -0400

    Basic logic of Smart Teleoperation

--- src/stdr_driver_node_code.cpp
+++ src/stdr_driver_node_code.cpp
@@ -4,57 +4,106 @@
 #include "sensor_msgs/LaserScan.h"
 #include <sstream>
 
-// Pointer Definition
-ros::Publisher *p_cmd_vel_pub;
+#define PI 3.1415926
 
+// Global Variabies Definition
+sensor_msgs::LaserScan laser_data;
+geometry_msgs::Twist cmd_vel;
 
 // Callback Function Definition
 void desiredVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
+  cmd_vel = *msg;
   ROS_INFO("Forward velocity %2.2f; Angular velocity %2.2f", msg->linear.x, msg->linear.z);
-  p_cmd_vel_pub->publish(*msg);
 }
 
 void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  ROS_INFO("LaserScan sequence number is: %i", msg->header.seq);
+  /*
+  Format of sensor_msgs/LaserScan
+  std_msgs/Header header
+    uint32 seq
+    time stamp
+    string frame_id
+  float32 angle_min
+  float32 angle_max
+  float32 angle_increment
+  float32 time_increment
+  float32 scan_time
+  float32 range_min
+  float32 range_max
+  float32[] ranges
+  float32[] intensities
+  */
+  laser_data = *msg;
 }
-
 
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "stdr_driver_node");
 
   // Init the node
   ros::NodeHandle n;
 
   // Init the subscriber
-  ros::Subscriber sub = n.subscribe("des_vel", 1000, desiredVelCallback);
-  ros::Subscriber sub1 = n.subscribe("robot0/laser_1", 1000, laserScanCallback);
+  ros::Subscriber sub = n.subscribe("robot0/laser_1", 1000, laserScanCallback);
   
   // Init the publisher
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("robot0/cmd_vel", 1000);
 
-
-  p_cmd_vel_pub = &cmd_vel_pub;
-
-  // See the frequency of loop in the node
+  // Set the frequency of loop in the node
   ros::Rate loop_rate(10);
+
+  float min_distance = 0.7;
+  float detect_angle = 70;
+  bool almost_hit = false;
 
   // Main logic
   while (ros::ok())
   {
-    geometry_msgs::Twist cmd_vel;
+    almost_hit = false;
 
-    cmd_vel.linear.x = 0;
+    // Velocity initial
+    cmd_vel.linear.x = 0.1;
     cmd_vel.linear.y = 0;
     cmd_vel.linear.z = 0;
     cmd_vel.angular.x = 0;
     cmd_vel.angular.y = 0;
-    cmd_vel.angular.z = 0.1;
+    cmd_vel.angular.z = 0;
 
-    // cmd_vel_pub.publish(cmd_vel);
+
+    int range_length = ceil((laser_data.angle_max-laser_data.angle_min)/laser_data.angle_increment);
+
+    /*  Hit Judgement 
+        Default scan angle from -135 to 135(-2.35 to 2.35)
+        we only care about the -70 to 70(face front)
+     */
+    for (int i=0; i<range_length; i++)
+    {
+      float scan_angle = laser_data.angle_min + i*laser_data.angle_increment;
+      if(scan_angle >= -detect_angle * PI / 180 && scan_angle <= detect_angle * PI / 180)
+      {
+         if (laser_data.ranges[i] < min_distance)
+         {
+            almost_hit = true;
+         }
+      }
+    }
+
+    if (almost_hit)
+    {
+      cmd_vel.linear.x = 0;
+      cmd_vel.angular.z = 0.2;
+      ROS_INFO("ALMOST HIT, START TURNING");
+    }
+    else{
+      cmd_vel.linear.x = 0.1;
+      cmd_vel.angular.z = 0;
+      ROS_INFO("HAVE ENOUGH DISTANCE, CONTINUE TO MOVE");
+    }
+
+    cmd_vel_pub.publish(cmd_vel);
 
     ros::spinOnce();
 
