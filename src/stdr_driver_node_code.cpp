f9a349f9da4068579d668f024d83bfd1ccc790dc
Haozheng-Li <haozheng.l@outlook.com>
2022-10-11 00:28:34 -0400

    Update the namespace

--- src/stdr_driver_node_code.cpp
+++ src/stdr_driver_node_code.cpp
@@ -9,12 +9,14 @@
 // Global Variabies Definition
 sensor_msgs::LaserScan laser_data;
 geometry_msgs::Twist cmd_vel;
+bool manual_control = false;
 
 // Callback Function Definition
 void desiredVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
+  manual_control = true;
   cmd_vel = *msg;
-  ROS_INFO("Forward velocity %2.2f; Angular velocity %2.2f", msg->linear.x, msg->linear.z);
+  ROS_INFO("Forward velocity %2.2f; Angular velocity %2.2f", msg->linear.x, msg->angular.z);
 }
 
 void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
@@ -47,15 +49,16 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   // Init the subscriber
-  ros::Subscriber sub = n.subscribe("robot0/laser_1", 1000, laserScanCallback);
+  ros::Subscriber sub = n.subscribe("des_vel", 1000, desiredVelCallback);
+  ros::Subscriber sub1 = n.subscribe("laser_1", 1000, laserScanCallback);
   
   // Init the publisher
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("robot0/cmd_vel", 1000);
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
 
   // Set the frequency of loop in the node
   ros::Rate loop_rate(10);
 
-  float min_distance = 0.7;
+  float min_distance = 0.5;
   float detect_angle = 70;
   bool almost_hit = false;
 
@@ -71,7 +74,6 @@ int main(int argc, char **argv)
     cmd_vel.angular.x = 0;
     cmd_vel.angular.y = 0;
     cmd_vel.angular.z = 0;
-
 
     int range_length = ceil((laser_data.angle_max-laser_data.angle_min)/laser_data.angle_increment);
 
@@ -96,10 +98,15 @@ int main(int argc, char **argv)
       cmd_vel.linear.x = 0;
       cmd_vel.angular.z = 0.2;
       ROS_INFO("ALMOST HIT, START TURNING");
+
+      manual_control=false; // When the robot get too close to the wall, switch to auto control
     }
     else{
-      cmd_vel.linear.x = 0.1;
-      cmd_vel.angular.z = 0;
+      if (!manual_control){
+        cmd_vel.linear.x = 0.1;
+        cmd_vel.angular.z = 0;
+      }
+
       ROS_INFO("HAVE ENOUGH DISTANCE, CONTINUE TO MOVE");
     }
 
